c147800a8855e8a47ae3d5a19311aa0225e49f04
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-10-15 10:18:45 +0530

    Add webpack make both environment dev and prod

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,12 @@
+import "./style.css";
+import Image from "./react.png";
+
 const App = () => {
     return (
-        <h1>React Typescript Webpack</h1>
+        <>
+            <h1>React Typescript Webpack - {process.env.NODE_ENV} - {process.env.name}</h1>
+            <img src={Image} alt="" width={300} />
+        </>
     );
 }
 
